4c89d4e9fe730d64fe981b515c08ed70fdeceaf2
ThomasLee <thomasl8484@gmail.com>
2025-08-13 14:28:35 +0800

    testSubtraction added

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -8,7 +8,13 @@ public class CalcTest {
 
     @Test
     void testAddition() {
+
         assertEquals(4, c.add(2,2));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2));
     }
 
 }
